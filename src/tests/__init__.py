5108acc5c52a792957b34ee615dc64fadd6a3119
rky95813 <thomas.fish@diamond.ac.uk>
2024-11-11 11:53:55 +0000

    fix ruff error

--- src/tests/__init__.py
+++ src/tests/__init__.py
@@ -4,4 +4,4 @@ import unittest
 def runall():
     # Includes integration tests
     suite = unittest.TestLoader().discover(".", pattern="test_*.py")
-    result = unittest.TextTestRunner(verbosity=2).run(suite)
+    unittest.TextTestRunner(verbosity=2).run(suite)
